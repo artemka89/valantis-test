bbf90d8857763e9be1850ff28c9ad4cf096a6b7b
Artem Moshniaga <artem.m89@mail.ru>
2024-03-04 19:24:45 +0300

    update path to logo.svg

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -8,7 +8,7 @@ export const RootLayout: FC = () => {
         <Link to="/">
           <div className="flex items-center">
             <div className="h-8 w-8">
-              <img src="/logo.svg" alt="logo" />
+              <img src="/valantis-test/logo.svg" alt="logo" />
             </div>
             <div className="text-xl font-bold text-cyan-700">VALANTIS</div>
           </div>
